7849a89012a06bdf9a2f67cd8f608c483df40900
developiaa <developiaa@gmail.com>
2022-08-20 21:59:29 +0900

    first commit

---
+++ src/test/java/study/developia/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.developia.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
